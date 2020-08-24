72e7a7ba5ccf952b3e666c00c11f2f3918c18492
BVadimV <59772240+BVadimV@users.noreply.github.com>
2020-08-24 19:23:28 +0300

    new webview app

---
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.test.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled
+        webView.loadUrl("https://navsegda.net/")
+    }
+}
